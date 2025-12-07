d94f6c0eb2f46994a5592b34d8d3f22f3359b58b
Pataki Dávid Ferenc <pataki.david.ferenc@gmail.com>
2025-12-07 21:55:28 +0100

    refactor: add useEffect to handle authentication checks and toast notifications

--- src/client/lib/ForceAuthenticated.ts
+++ src/client/lib/ForceAuthenticated.ts
@@ -1,12 +1,22 @@
 "use client";
 import { addToast } from "@heroui/react";
 import { useRouter } from "next/navigation";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "../components/contexts/AuthContext";
 
 export function forceAuthenticated(): boolean {
     const authContext = useContext(AuthContext);
     const router = useRouter();
+
+    useEffect(() => {
+        if (!authContext.loading && !authContext.user) {
+            addToast({
+                title: "You must be logged in to access this page.",
+                color: "warning",
+            });
+            router.push("/auth");
+        }
+    }, [authContext.loading, authContext.user, router]);
 
     if (!authContext.loading && !authContext.user) {
         addToast({
